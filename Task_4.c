1603fa688087122c74a75c28a84160af67f480fb
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-28 00:48:07 +0530

    comparison_operators

---
+++ Task_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+	int x , y;
+
+	printf("value of X is :");
+
+	scanf("%d",&x);
+
+	printf("value of Y is :");
+
+	scanf("%d",&y);
+
+	printf("%d",x!=y);
+
+	return 0;
+
+}
